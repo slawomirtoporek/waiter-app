425f4814984513f2f4b47e7e5d444e55265cefdf
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-02 23:55:21 +0200

    init project

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import initialState from './initialState';
+import tablesReducer from './tablesRedux';
+import thunk from 'redux-thunk';
+
+const subreducers = {
+  tables: tablesReducer,
+};
+
+const reducer = combineReducers(subreducers);
+
+const store = createStore(
+  reducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+  ),
+);
+
+export default store;
---
+++ src/redux/tablesRedux.js
@@ -0,0 +1,14 @@
+// selectors
+
+// actions
+
+// action creators
+
+const tablesReducer = (statepart = [], action) => {
+  switch (action.type) {
+    default:
+      return statepart
+  };
+};
+
+export default tablesReducer;
